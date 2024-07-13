efbb58714325545ea600a00a633ba043caeaaa37
dartiros <realdarter1234@gmail.com>
2024-07-12 21:12:38 -0700

    Modularized genereting and training functions
    Made it so avg loss over 20 and fixed issues. Modularized 2 functions

--- model_utils.py
+++ model_utils.py
@@ -1,10 +1,10 @@
 import torch
 from torch.utils.data import Dataset, DataLoader
 from transformers import GPT2LMHeadModel, GPT2Tokenizer
-import os
 from tokenization import *  # Import the tokenizer wrapper from encoder_decoder.py
 from file_utils import *
 import time
+from collections import deque
 
 CUDA_LAUNCH_BLOCKING=1
 
@@ -13,10 +13,6 @@ class CustomDataset(Dataset):
         self.input_ids = input_ids
         self.attention_masks = attention_masks
         self.labels = labels
-        print(f"Dataset size: {len(self.input_ids)}")  # Add this line
-        print(f"Dataset tokens: {(self.input_ids)}")  # Add this line
-        print(f"labels size: {len(self.labels)}")  # Add this line
-        print(f"labels tokens: {(self.labels)}")  # Add this line
     def __len__(self):
         return len(self.input_ids)
 
@@ -27,69 +23,67 @@ class CustomDataset(Dataset):
             'labels': self.labels[idx]
         }
 
-def check_gpt2_models_exist(model_path):
-    model_files = [
-        'added_tokens.json',
-        'config.json',
-        'generation_config.json',
-        'merges.txt',
-        'model.safetensors',
-        'special_tokens_map.json',
-        'tokenizer_config.json',
-        'vocab.json'
-    ]
-    all_files_exist = True
-    for file in model_files:
-        file_path = os.path.join(model_path, file)
-        absolute_path = os.path.abspath(file_path)  # Get the absolute path
-        if not os.path.isfile(absolute_path):
-            print(f"File missing: {absolute_path}")
-            all_files_exist = False
-    return all_files_exist
+def load_model_and_tokenizer(model_directory):
+    model = GPT2LMHeadModel.from_pretrained(model_directory)
+    tokenizer = GPT2Tokenizer.from_pretrained(model_directory)
+    model.resize_token_embeddings(len(tokenizer))
+
+    return model, tokenizer
 
 def download_gpt2_124M(save_directory):
     if check_gpt2_models_exist(save_directory):
         print("Model already exists. Not downloading.")
         return False
-    global model, tokenizer
+    model = GPT2LMHeadModel.from_pretrained('gpt2')
+    tokenizer = GPT2Tokenizer.from_pretrained('gpt2')
+
     model.save_pretrained(save_directory)
     tokenizer.save_pretrained(save_directory)
     print(f"GPT-2 model (124M) downloaded and saved in {save_directory}")
     return True
 
+def __print_training_progress__(epoch, num_epochs, i, len_dataloader, loss, avg_loss, start_time, total_steps):
+    elapsed_time = time.time() - start_time
+    steps_completed = epoch * len_dataloader + i
+    steps_remaining = total_steps - steps_completed
+    avg_time_per_step = elapsed_time / steps_completed
+    estimated_time_remaining = avg_time_per_step * steps_remaining
+
+    print(f"Epoch [{epoch+1}/{num_epochs}], Step [{i}/{len_dataloader}], Loss: {loss:.4f}, Avg Loss: {avg_loss:.4f}, Elapsed Time: {elapsed_time:.2f} seconds, Estimated Time Remaining: {estimated_time_remaining:.2f} seconds")
+
 def train_model(model_directory, dataset_array=[], num_epochs=1, batch_size=1, save_every=500):
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     print(f"Using device: {device}")
 
-    model = GPT2LMHeadModel.from_pretrained(model_directory)
-    tokenizer = GPT2Tokenizer.from_pretrained(model_directory)
-    
+    model, tokenizer = load_model_and_tokenizer(model_directory)
+
+    ensure_tokens(tokenizer)
     model.resize_token_embeddings(len(tokenizer))
     model.to(device)
-    
-    print("Loading Data...")
-    textlines = dataset_array
-    input_ids, attention_masks, labels = tokenize_dataset(tokenizer, textlines)
-    dataset = CustomDataset(input_ids, attention_masks, labels)
-    dataloader = DataLoader(dataset, batch_size=batch_size, shuffle=True)
-    print("Finished Loading Data")
 
     optimizer = torch.optim.Adam(model.parameters(), lr=5e-5)
     scaler = torch.cuda.amp.GradScaler()
 
-    model.train()
+    input_ids, attention_masks, labels = tokenize_dataset(tokenizer, dataset_array)
+    dataset = CustomDataset(input_ids, attention_masks, labels)
+    dataloader = DataLoader(dataset, batch_size=batch_size, shuffle=True)
 
-    start_time = time.time()  # Start timing the training process
     total_steps = len(dataloader) * num_epochs
+    start_time = time.time()
 
-    iterations = 0
+    model.train()
+    recent_losses = deque(maxlen=20)  # Keeps track of last 20 losses
+
     for epoch in range(num_epochs):
         total_loss = 0
-        optimizer.zero_grad()
-        for i, batch in enumerate(dataloader):
+        epoch_start_time = time.time()
+
+        for i, batch in enumerate(dataloader, 1):
             input_ids = batch['input_ids'].to(device)
             attention_mask = batch['attention_mask'].to(device)
             labels = batch['labels'].to(device)
+
+            optimizer.zero_grad()
 
             with torch.cuda.amp.autocast():
                 outputs = model(input_ids, attention_mask=attention_mask, labels=labels)
@@ -98,38 +92,41 @@ def train_model(model_directory, dataset_array=[], num_epochs=1, batch_size=1, s
             scaler.scale(loss).backward()
             scaler.step(optimizer)
             scaler.update()
-            optimizer.zero_grad()
-            
-            total_loss += loss.item()
-            avg_loss = total_loss / (i + 1)
 
             total_loss += loss.item()
-            elapsed_time = time.time() - start_time
-            steps_completed = epoch * len(dataloader) + i + 1
-            steps_remaining = total_steps - steps_completed
-            avg_time_per_step = elapsed_time / steps_completed
-            estimated_time_remaining = avg_time_per_step * steps_remaining
+            avg_loss = total_loss / i
 
-            print(f"Epoch [{epoch+1}/{num_epochs}], Step [{i+1}/{len(dataloader)}], Loss: {loss.item():.4f}, Avg Loss: {avg_loss:.4f}, Elapsed Time: {elapsed_time:.2f} seconds, Estimated Time Remaining: {estimated_time_remaining:.2f} seconds")
+            # Append current loss to recent_losses
+            recent_losses.append(loss.item())
 
-            iterations += 1
-            if iterations % save_every == 0:
+            __print_training_progress__(epoch, num_epochs, i, len(dataloader), loss.item(), avg_loss, start_time, total_steps)
+
+            if i % save_every == 0:
                 model.save_pretrained(model_directory)
-                print(f"Model saved at iteration {iterations} in {model_directory}")
+                print(f"Model saved at iteration {i} in {model_directory}")
 
         avg_loss = total_loss / len(dataloader)
-        print(f"Epoch {epoch+1} completed. Average Loss: {avg_loss}")
-        
+        print(f"Epoch {epoch+1} completed. Average Loss: {avg_loss:.4f}")
+
+        epoch_elapsed_time = time.time() - epoch_start_time
+        print(f"Epoch {epoch+1} completed in {epoch_elapsed_time:.0f} seconds.")
+
         model.save_pretrained(model_directory)
-        print(f"Model saved")
 
-    end_time = time.time()  # End timing after training completes
-    elapsed_time = end_time - start_time
+    total_elapsed_time = time.time() - start_time
+    print(f"Training completed in {total_elapsed_time // 60:.0f} minutes and {total_elapsed_time % 60:.0f} seconds.")
 
-    print(f"Training completed in {elapsed_time // 60:.0f} minutes and {elapsed_time % 60:.0f} seconds.")
 
-    model.save_pretrained(model_directory)
-    print(f"Final model saved in {model_directory}")
+
+
+
+
+
+
+
+
+
+
 
 def test_input(model_directory, prompt_text, max_length=50, temperature=0.7, top_k=50, top_p=0.95, repetition_penalty=2.5):
     # Check if GPU is available and use it if possible
@@ -137,14 +134,13 @@ def test_input(model_directory, prompt_text, max_length=50, temperature=0.7, top
     print(f"Using device: {device}")
 
     # Load the trained model and tokenizer
-    model = GPT2LMHeadModel.from_pretrained(model_directory)
-    tokenizer = GPT2Tokenizer.from_pretrained(model_directory)
+    model, tokenizer = load_model_and_tokenizer(model_directory)
 
     # Move the model to the specified device (GPU or CPU)
     model.to(device)
 
     # Add special tokens to the tokenizer if they are not already present
-    special_tokens_dict = {'pad_token': '[PAD]', 'sep_token': '<|septext|>', 'eos_token': '<|endoftext|>', 'bos_token': '<|startoftext|>'}
+    special_tokens_dict = {'pad_token': '[PAD]', 'sep_token': '<[SEP]>', 'eos_token': '<[EOS]>', 'bos_token': '<[BOS]>'}
     num_added_toks = tokenizer.add_special_tokens(special_tokens_dict)
     model.resize_token_embeddings(len(tokenizer))
 
@@ -198,29 +194,3 @@ def generate_responses(model_directory, prompt_text, max_length=50, temperature=
 
     return [before_tsep.strip(), after_tsep.strip()]
 
-
-"""
-if __name__ == "__main__":
-    model_path = 'checkpoint/run1'
-    csv_path = os.path.join(model_path, 'cleaned.csv')
-    encoded_txt_path = os.path.join(model_path, 'csv_encoded.txt')
-    batch_size = 1  # Reduce batch size to fit within memory
-    if (ensure_file_exists(csv_path, create_if_missing=False)):
-        encode_csv(csv_path, encoded_txt_path, header=True)
-    else:
-        exit()
-
-    # Load pre-trained model and tokenizer before attempting to save them
-    model_name_or_path = 'gpt2'
-    #check_gpt2_models_exist(save_path)
-    download_gpt2_124M(save_path)
-
-
-    # Uncomment this line to train the model
-    #train_on_dataset(save_path, encoded_txt_path, num_epochs=1, batch_size=3)
-
-    while True:
-        test_prompt = input("Input: ")
-        test_prompt = f"<|startoftext|> {test_prompt} <|septext|>"
-        test_input(model_path, test_prompt)
-"""
