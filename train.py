c9b56bcf508843bcb7a4906a4930552d23a62880
dartiros <realdarter1234@gmail.com>
2024-07-12 21:08:53 -0700

    Updated Train Script
    Edited Train for updated modul_utils

--- train.py
+++ train.py
@@ -1,17 +1,16 @@
 from model_utils import *
+from file_utils import *
 import os
 CUDA_LAUNCH_BLOCKING=1
 
 if __name__ == "__main__":
     model_path = 'checkpoint/run1'
     csv_path = os.path.join(model_path, 'cleaned.csv')
-    csv_path = 'cleaned.csv'
-    #encoded_txt_path = os.path.join(model_path, 'csv_encoded.txt')
 
-
+    # Prepare the CSV data
     encoded_data = prepare_csv(csv_path)
-    print(encoded_data[0])
-    print(encoded_data[-1])
 
+    download_gpt2_124M(model_path)
+    # Train the model
     train_model(model_path, encoded_data, num_epochs=10, batch_size=3, save_every=100)
 
